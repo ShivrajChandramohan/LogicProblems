e49ff99cc7782e3d9a549d0df64c6aacb18b63b7
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 23:04:39 +0530

    Find Prime

---
+++ Primenumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicproblems
+{
+    public class Primenumber
+    {
+        public static void Findprime()
+        {
+            int i, n, a;
+
+            Console.WriteLine("Enter Number:");
+            
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 2; i <= n / 2; i++)
+            {
+                a = n % i;
+
+
+                if (a == 0)
+                {
+                    Console.WriteLine("The given number is not a prime number");
+                }
+
+                else
+                {
+                    Console.WriteLine("The given number is a prime number");
+                }
+            }
+        }
+    }
+}
