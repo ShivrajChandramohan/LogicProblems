248c9c03133a9be71219b9b17e4f21e41002737e
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 23:03:01 +0530

    Findnumber

---
+++ perfectnumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicproblems
+{
+    public class perfectnumber
+    {
+        public static void Findnumbers()
+        {
+            int n, i, sum;
+            int mn, mx;
+
+            Console.WriteLine("Find the perfect numbers within a given number of range");
+
+            Console.WriteLine("Input the Starting range");
+
+            mn=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the ending range");
+
+            mx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The perfect number within the range:");
+
+            for(n=mn; n<=mx; n++)
+            {
+                i = 1;
+                sum=0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.Write(" {0} ", n);
+            }
+        }
+        
+    }
+}
