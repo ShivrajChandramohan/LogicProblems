b5fdadf045b342d73f033d60d9b9f46e0e451524
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 22:55:13 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Logicproblems
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to logic problems");
+
+        }
+    }
+}
